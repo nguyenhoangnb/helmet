3664e2f38f6c35bec37a75d3e777d749f9a7dc8b
nguyenhoangnb <nguyenhuyhoang16yl@gmail.com>
2026-05-03 23:39:50 +0700

    fix readme.md

---
+++ README.md
@@ -0,0 +1,15 @@
+### TEST
+```bash
+streamlit run app/main.py
+```
+
+## run sever
+```bash
+python3 get_follower.py
+```
+
+
+## run stream camera
+```bash
+python3 stream_cam.py
+```
--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ if 'report_data' not in st.session_state:
 @st.cache_resource
 def load_model():
     with st.spinner("🚀 Đang tải mô hình YOLO..."):
-        return YOLO("weights/bestyolo.pt")
+        return YOLO("weights/best_helmet.pt")
 
 model = load_model()
 
